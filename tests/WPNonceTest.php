31fed83b75cae1282f0c017e60feee609a640f10
Jeff <none>
2018-02-05 23:04:01 +0000

    add test

---
+++ tests/WPNonceTest.php
@@ -0,0 +1,30 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+final class WPNonceTest extends TestCase
+{
+
+
+    public function testCanDisplayMessage()
+    {
+        $this->assertInstanceOf(
+            WPNonce::class,
+            WPNonce::ays('Are you sure you want to do this?')
+        );
+    }
+
+    /*public function testCannotDisplayMessage()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        WPNonce::ays();
+    }
+
+    public function testLogout()
+    {
+        $this->assertEquals(
+            '',
+            WPNonce::ays('log-out')
+        );
+    }*/
+}
